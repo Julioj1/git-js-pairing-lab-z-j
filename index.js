1ef53ac6349c8784a851549645d0a9465477b06d
ZaneC04 <zenithzsc@gmail.com>
2025-10-01 15:01:13 -0400

    second function solved

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const fiveToOneHundred = () => {
     }
 }
 fiveToOneHundred();
+
+const multiplesOfThree = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+    }
+}
+multiplesOfThree();
