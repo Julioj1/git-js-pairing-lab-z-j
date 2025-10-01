0897ca075ead27ddf1b9ecee202dccc3725a4946
Julioj1 <dbjulioj@gmail.com>
2025-10-01 14:52:43 -0400

    First function done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Code your solutions in this file
+// first function fiveToOneHundred
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++) {
+        console.log(i)
+    }
+}
+fiveToOneHundred();
