ec2924d94142a94ce80ff78d7285168fb76606e1
Julioj1 <dbjulioj@gmail.com>
2025-10-02 15:45:48 -0400

    Added final function

--- index.js
+++ index.js
@@ -127,3 +127,25 @@ const switchLetters = (str) => {
 };
 // console.log(switchLetters(""));
 
+function changeString(str) {
+    var result = "";
+    for (var i = 0; i < str.length; i++) {
+        // handle "z"
+        if (122 == str.charCodeAt(i)) {
+            result += "a";
+            // handle "Z"
+        } else if (90 == str.charCodeAt(i)) {
+            result += "A";
+            // handle all other letter characters
+        } else if ((65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 89) ||
+            (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 121)) {
+            result += String.fromCharCode(str.charCodeAt(i) + 1);
+            // append all other characters unchanged
+        } else {
+            result += str.charAt(i);
+        }
+    }
+    return result;
+}
+
+console.log(changeString("abcd"));
